c3632db5f30e469e338a798bcf2c059d7bacd504
jhernandez2021111 <jhernandez-2021111@kinal.edu.gt>
2023-04-29 09:06:07 -0600

    Se agrego toda la estrcutura del proyecto

---
+++ BackEnd/configs/app.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet'); 
+const cors = require ('cors');
+const app = express();
+//constante del puerto
+const port = process.env.PORT || 3200;  
+
+
+
+//configurar el servidor de http en express
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(cors());-
+app.use(helmet());
+app.use(morgan('dev'));
+
+//rutas de cada coleccion
+
+
+
+//fucion para levantar el servidor
+exports.initServer = ()=>{
+    app.listen(port);
+    console.log(`Server http running in port ${port}`);
+}
