7c9744defb79a5541e6f0004fac81fc19b483400
jhernandez2021111 <jhernandez-2021111@kinal.edu.gt>
2023-04-29 17:12:41 -0600

    Se agrego funcionamientos de user

--- BackEnd/src/users/user.routes.js
+++ BackEnd/src/users/user.routes.js
@@ -7,6 +7,9 @@ const {ensureAuth, isAdmin} = require('../services/authenticated');
 
 api.get('/', userController.test);
 api.post('/registerUser', userController.registerUser);
-api.post('/login',userController.login)
+api.post('/login',userController.login);
+api.put('/updateOwnUser', ensureAuth,userController.updateOwnUser);
+api.delete('/deleteOwnUser', ensureAuth, userController.deleteOwnUser);
+api.get('/get',[ensureAuth,isAdmin], userController.get)
 
 module.exports = api;
