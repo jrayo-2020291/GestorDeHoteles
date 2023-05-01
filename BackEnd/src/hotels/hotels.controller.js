3556235589409ace1b613b8d506fbe78f734e388
jhernandez2021111 <jhernandez-2021111@kinal.edu.gt>
2023-04-30 20:07:33 -0600

    Agrego funciones de hoteles

--- BackEnd/src/hotels/hotels.controller.js
+++ BackEnd/src/hotels/hotels.controller.js
@@ -1,7 +1,8 @@
 'use strict'
 
 const Hotel = require('./hotels.model');
-const Category = require('../hotels/hotels.model');
+const Event = require('../events/events.model');
+const {validateData} = require('../utils/validate')
 const mongoose = require('mongoose');
 
 exports.test = (req, res)=>{
@@ -11,14 +12,16 @@ exports.test = (req, res)=>{
 exports.add = async(req,res)=>{
     try {
         let data = req.body;
+        let validate = validateData(data);
+        if(validate) return res.status(400).send({validate})
         let hotel = new Hotel(data);
-        await hotel.save()
+        await hotel.save();
         return res.send({message:'hotel created sucessfully', hotel});
     } catch (err) {
         console.error(err);
         return res.status(500).send({message:'Error adding Hotel'})    
     }
-}
+};
 
 exports.get = async(req,res)=>{
     try {
@@ -29,3 +32,91 @@ exports.get = async(req,res)=>{
         return res.status(500).send({message:'Error get Hotels'})
     }
 }
+
+exports.addEvent = async(req,res)=>{
+    try {
+        let hotelId = req.params.id;
+        let event = await Event.findOne({_id:req.body.event});
+        let hotel = await Hotel.findOne({_id:hotelId});
+    for(let i=0;i<hotel.events.length;i++){
+        const idEnString = hotel.events[i].toString();
+        if(req.body.event===idEnString)
+        return res.send({message:'This event is already registered at the hotel'})
+    } 
+        if(!event) return res.send({message:'Event not found'})
+        let updateHotel = await Hotel.findOneAndUpdate(
+            {_id:hotelId},
+            {$push:{events:req.body.event}},
+            {new:true});
+        if(!updateHotel) return res.send({message:'hotel not found and not update'});
+        return res.send({message:'updating sucessfully',updateHotel})
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send({message:'Error add Event to Hotel'})
+    }
+};
+
+exports.deleteEvent = async(req,res)=>{
+    try {
+        let hotelId = req.params.id;
+        let event = await Event.findOne({_id:req.body.event});
+        if(!event) return res.send({message:'Event not found'})
+        let updateHotel = await Hotel.findOneAndUpdate(
+            {_id:hotelId},
+            {$pullAll:{events:[req.body.event]}},
+            {new:true});
+        if(!updateHotel) return res.send({message:'hotel not found and not update'});
+        return res.send({message:'updating sucessfully',updateHotel})
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send({message:'Error deleting event to hotel'})
+    }
+}
+
+exports.getByName = async(req,res)=>{
+    try {
+        let data = req.body;
+        if(data.name==='') return res.send({message:'You must enter a name'});
+        let hotels = await Hotel.find({
+            name:{
+                $regex: data.name, 
+                $options: 'i'
+            }
+        },{__v:0}).populate('events',['name']);
+        return res.send(hotels);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send({message:'Error get hotels'})
+    }
+};
+
+exports.getByLocation = async(req,res)=>{
+    try {
+        let data = req.body;
+        if(data.location==='') return res.send({message:'You must enter a location'});
+        let hotels = await Hotel.find({
+            locationH:{
+                $regex: data.location, 
+                $options: 'i'
+            }
+        },{__v:0}).populate('events',['name']);
+        return res.send(hotels);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send({message:'Error get hotels'})
+    }
+};
+
+exports.getById = async(req,res)=>{
+    try {
+        let hotelId = req.params.id;
+        let data = req.body;
+        if(data.location==='') return res.send({message:'You must enter a location'});
+        let hotel = await Hotel.findOne({_id:hotelId},{__v:0}).populate('events',['name']);
+        if(!hotel) return res.send({message:'Hotel not found'})
+        return res.send(hotel);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send({message:'Error get hotels'})
+    }
+};
--- BackEnd/src/hotels/hotels.routes.js
+++ BackEnd/src/hotels/hotels.routes.js
@@ -9,5 +9,10 @@ const {ensureAuth, isAdmin}=require('../services/authenticated')
 api.get('/', hotelsController.test);
 api.post('/addHotel', [ensureAuth,isAdmin], hotelsController.add)
 api.get('/get', hotelsController.get)
+api.get('/getById/:id',[ensureAuth,isAdmin],hotelsController.getById)
+api.post('/getByName', ensureAuth, hotelsController.getByName)
+api.post('/getbyLocation',ensureAuth,hotelsController.getByLocation)
+api.put('/addEvent/:id',[ensureAuth,isAdmin], hotelsController.addEvent)
+api.put('/deleteEvent/:id',[ensureAuth,isAdmin], hotelsController.deleteEvent)
 
 module.exports = api;
