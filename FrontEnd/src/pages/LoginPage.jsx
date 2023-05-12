9ffd7b768b3896714c7a19784758a554fba6c744
jrayo-2020291 <jrayo-2020291@knal.edu.gt>
2023-05-12 16:06:18 -0600

    Se regresó el lógin

--- FrontEnd/src/pages/LoginPage.jsx
+++ FrontEnd/src/pages/LoginPage.jsx
@@ -1,43 +1,60 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import React, { useState, useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import Swal from 'sweetalert2';
+
 
 export const LoginPage = () => {
-    const navigate = useNavigate()
-    const [form, setForm] = useState({
-        username: '',
-        password: ''
+  const navigate = useNavigate()
+  const [form, setForm] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleChange = (e)=>{
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
     })
+  }
 
-    const handleChange = (e) => {
-        setForm({
-            ...form,
-            [e.target.name]: e.target.value
+  const login = async(e)=>{
+    try{
+      e.preventDefault()  
+      const { data } = await axios.post('http://localhost:3100/user/login', form)
+      if(data.token){
+       localStorage.setItem('token', data.token)
+       localStorage.setItem('role', data.user.role)
+        Swal.fire({
+          title: data.message || 'Login successfully',
+          icon: 'success',
+          timer: 2000
         })
+        navigate('/dashboard')
+      }
+    }catch(err){
+      console.log(err)
+      Swal.fire('Error with login', err.response.data.message, 'error')
+      throw new Error('Error login failed')
     }
+  }
 
-    const login = async (e) => {
-        e.preventDefault()
-        try {
-            const { data } = await axios.post('http://localhost:3100/users/login', form)
-            if (data.token) {
-                setLoggedIn(true)
-                localStorage.setItem('token', data.token)
-                localStorage.setItem('role', data.user.role)
-                if (data.user.role === "ADMIN") {
-                    navigate('/dashboard')
-                } else {
-                    navigate('/worker')
-                }
-                window.location.reload()
-            }
-        } catch (err) {
-            console.log(err)
-            alert(err.response.data.message)
-            throw new Error('Error login failed')
-        }
-    }
-    return (
-        <div>Login</div>
-    )
+  return (
+    <>
+    <div className='container'>
+    <h2 className='text-center'>Log In</h2>
+      <form className='m-5 text-center'>
+        <div className='mb-3'>
+          <label className='form-label' htmlFor="">Username</label>
+          <input onChange={handleChange} name='username' className='form-control' type="text" />
+        </div>
+        <div className='mb-3'>
+          <label className='form-label' htmlFor="">Password</label>
+          <input onChange={handleChange} name='password' className='form-control' type="password" />
+        </div>
+          <button onClick={(e)=> login(e)} className='btn btn-success'>Login</button>
+      </form>
+    </div>
+    </>
+  )
 }
