efe91609d37ff0d86ac0f874ca7bab4e9d737e45
Eduardo Hernandez <ehernandez-2021591@kinal.edu.gt>
2023-05-16 11:51:40 -0600

    Se hizo el crud para añadir Servicios

--- BackEnd/src/additionalServices/additionalService.routes.js
+++ BackEnd/src/additionalServices/additionalService.routes.js
@@ -7,11 +7,11 @@ const {ensureAuth, isAdmin} = require('../services/authenticated');
 
 api.get('/test', servicesController.test);
 api.post('/addService', [ensureAuth, isAdmin],servicesController.addService);
-api.get('/getServices',  ensureAuth,  servicesController.getServices);
+api.get('/getServices',  ensureAuth , servicesController.getServices);
 api.get('/get/:id',  ensureAuth,  servicesController.getServiceById);
 api.post('/search',  ensureAuth,   servicesController.getByName);
 api.put('/update/:id',  [ensureAuth, isAdmin],  servicesController.update);
 api.delete('/delete/:id',  [ensureAuth, isAdmin] ,  servicesController.delete);
-api.get('/getcategory',  ensureAuth,  servicesController.getByCategory);
+api.post('/getcategory',  ensureAuth,  servicesController.getByCategory);
 
 module.exports = api;
--- FrontEnd/src/components/ReservationEvent/addService.jsx
+++ FrontEnd/src/components/ReservationEvent/addService.jsx
@@ -5,18 +5,21 @@ import { Aservice } from '../AServices/Aservice'
 
 export const AddService = () => {
     const [services, setServices] = useState([{}])
-    const [reservation, setReservations] = useState([{}])
+    const [reservation, setReservation] = useState([{}])
     const token = localStorage.getItem('token')
     const navigate = useNavigate()
     const { id } = useParams()
 
     const getServices = async () => {
       try {
-        const { data } = await axios('http://localhost:3100/services/getServices', {
+        let form = {
+          category: 'ROOM'
+      }
+        const { data } = await axios.post('http://localhost:3100/services/getCategory', form,{
           headers: {
-            'Authorization': token
+              'Authorization': token
           }
-        })
+      })
         setServices(data.services)
         console.log(services)
       } catch (err) {
@@ -24,41 +27,40 @@ export const AddService = () => {
       }
     }
     const getReservation = async () => {
-        try {
-            const { data } = await axios.get(`http://localhost:3100/reservationEvent/getReservation/${id}`, {
+      try {
+          const { data } = await axios(`http://localhost:3100/reservationEvent/getReservation/${id}`, {
+              headers: {
+                  'Authorization': token
+              }
+          })
+          let array = []
+          let service = data.reservation.additionalServices
+          service.forEach(element=>{
+              array.push(element.service)
+          })
+          setReservation(array)
+      } catch (err) {
+          console.error(err)
+      }
+  }
+  const updateReservation = async (e) => {
+    try {
+        e.preventDefault()
+        let updatedReservation = {
+            service: document.getElementById('inputService').value
+        }
+        const { data } = await axios.put(`http://localhost:3100/reservationEvent/addService/${id}`, updatedReservation,
+            {
                 headers: {
                     'Authorization': token
                 }
             })
-            let array = []
-            let services = data.reservation.additionalServices
-            services.forEach(element=>{
-                array.push(element.services)
-            })
-            console.log(array)
-            setReservations(array)
-        } catch (err) {
-            console.error(err)
-        }
+        alert(`${data.message}`)
+        getServices()
+    } catch (err) {
+        console.error(err)
     }
-    const UpdateReservation = async (e) => {
-        try {
-            e.preventDefault()
-            let updatedReservatiom = {
-                services: document.getElementById('inputService').value
-            }
-            const { data } = await axios.put(`http://localhost:3100/reservationEvent/addService/${id}`, updatedReservatiom,
-                {
-                    headers: {
-                        'Authorization': token
-                    }
-                })
-            alert(`${data.message}`)
-            navigate('/dashboard/Lease')
-        } catch (err) {
-            console.error(err)
-        }
-    }
+}
 
     useEffect(() => getServices, [])
     useEffect(() => getReservation, [])
@@ -78,7 +80,7 @@ export const AddService = () => {
               </thead>
               <tbody>
                 {
-                  services.map(({ _id, name, description, cost}, index) => {
+                  reservation.map(({_id, name, description, cost}, index) => {
                     return (
                       <tr key={index}>
                         <Aservice
@@ -106,7 +108,7 @@ export const AddService = () => {
                             </select>
                         </div>
                         <br />
-                        <button onClick={(e) => UpdateReservation(e)} type="submit" className="btn btn-primary">Add</button>
+                        <button onClick={(e) => updateReservation(e)} type="submit" className="btn btn-primary">Add</button>
                         <Link to='/dashboard/Lease'>
                             <button type="submit" className="btn btn-primary">Cancel</button>
                         </Link>
