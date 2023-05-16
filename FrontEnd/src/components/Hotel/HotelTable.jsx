570a9325849b2dae31ca6af98c6082024f453d66
jhernandez2021111 <jhernandez-2021111@kinal.edu.gt>
2023-05-16 10:03:01 -0600

    Se escondieron botones segun rol y se realizo el serach de hoteles con cambios en el back

--- FrontEnd/src/components/Hotel/HotelTable.jsx
+++ FrontEnd/src/components/Hotel/HotelTable.jsx
@@ -4,10 +4,46 @@ import axios from 'axios'
 import {Hotel} from './Hotel'
 
 export const HotelTable = () => {
+    const role = localStorage.getItem('role')
     const [hotel,setHotel] = useState([{}])
+    const [show, setShow] = useState(false)
     const [loading, setLoading] = useState(true)
     const token = localStorage.getItem('token')
     const navigate = useNavigate()
+    const [search, setSearch] = useState({
+        name: ''
+    })
+
+    const restringir = () => {
+        if (role === 'ADMIN' ) {
+          setShow(true)
+        }
+    }
+
+    const searching = async()=>{
+        try{
+          const { data } = await axios.post('http://localhost:3100/hotel/getByName', search, {
+            headers: {
+              'Authorization': token
+            }
+          })
+          if(data.hotels){
+            setHotel(data.hotels)
+          } else if (search.name === ''){
+            getHotels()
+          } 
+        }catch(err){
+          console.error(err)
+        }
+      }
+
+    const handleChange = (e)=>{
+        setSearch({
+          ...search,
+          [e.target.name]: e.target.value
+        })
+        searching()
+    }
 
     const getHotels = async () => {
         try {
@@ -41,7 +77,8 @@ export const HotelTable = () => {
         }
       }
 
-      useEffect(() => getHotels, [])
+    useEffect(() => getHotels, [])
+    useEffect(() => restringir, [])
 
     return (
         <>
@@ -49,19 +86,34 @@ export const HotelTable = () => {
                 <main>
                     <h1 className="title">Hoteles</h1>
                         <ul className="breadcrumbs">
-                            <li><a href="#">Administrador</a></li>
+                            {
+                                show ? (
+                                    <li><a href="#">Administrador</a></li>
+                                ):(<li><a href="#">Usuario</a></li>)
+                            }
                             <li className="divider">/</li>
                             <li><a href="#" className="active">Gestor de Hoteles</a></li>
                         </ul>
-                    <br/> 
+                    <br/>
+                    <form action="#">
+                        <div className="form-group">
+                        <input name='name' onChange={handleChange} type="text" placeholder="Buscar..."/>
+                        <i className="fa-solid fa-magnifying-glass icon"></i>
+                        </div>
+                    </form>
+                    <br /> 
                     <div className="info-data">
                         <div className="menu">
                             <div className="sub-menu">
                             </div>
                             <br/>
-                            <Link to='../addHotel'>
-                                <i className="fa-solid fa-plus add"></i>
-                            </Link>
+                            {
+                                show ? (
+                                    <Link to='../addHotel'>
+                                        <i className="fa-solid fa-plus add"></i>
+                                    </Link>
+                                ):(<br></br>)
+                            }
                             <br/>
                             <br/>
                             <table>
@@ -71,8 +123,15 @@ export const HotelTable = () => {
                                         <th>Ubicación</th>
                                         <th>Calificación</th>
                                         <th>Habitaciones</th>
-                                        <th>Services</th>
-                                        <th>Acciones</th>
+                                        {
+                                            show ?(
+                                                <>
+                                                    <th>Eventos</th>
+                                                    <th>Acciones</th>
+                                                </>
+                                            ):(<th>Descripción</th>)
+                                        }
+                                        
                                     </tr>
                                 </thead>
                                     
@@ -87,17 +146,29 @@ export const HotelTable = () => {
                                                             qualification={qualification}
                                                             numberRooms={numberRooms}
                                                         ></Hotel>
-                                                        <td>
-                                                            <Link to={`../hotel/event/${_id}`}>
-                                                                <i className="fa-solid fa-clipboard button"></i>
-                                                            </Link>
-                                                        </td>
-                                                        <td>
-                                                            <Link to={`../updateHotel/${_id}`}>
-                                                                <i className="fa-solid fa-pen button"></i>
-                                                            </Link>
-                                                            <i onClick={() => deleteHotel(_id)} className="fa sharp fa-solid fa-trash button"></i> 
-                                                        </td>
+                                                        
+                                                        {show ?(
+                                                            <>
+                                                                <td>
+                                                                <Link to={`../hotel/event/${_id}`}>
+                                                                    <i className="fa-solid fa-clipboard button"></i>
+                                                                </Link>
+                                                                </td>
+                                                                <td>
+                                                                    <Link to={`../updateHotel/${_id}`}>
+                                                                        <i className="fa-solid fa-pen button"></i>
+                                                                    </Link>
+                                                                    <i onClick={() => deleteHotel(_id)} className="fa sharp fa-solid fa-trash button"></i> 
+                                                                </td>
+                                                            </>
+                                                            ):(
+                                                                <td>
+                                                                    <Link to={`../hotel/event/${_id}`}>
+                                                                        <i className="fa-solid fa-clipboard button"></i>
+                                                                    </Link>
+                                                                </td>
+                                                            )
+                                                        }
                                                     </tr>
                                                 )
                                             })
